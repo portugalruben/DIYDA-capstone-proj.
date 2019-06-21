131ef42056ae4a5831c4acb3d59ff87f464402b3
Ruben D. Portugal <portugalruben@hotmail.com>
2019-06-21 09:41:44 -0500

    first commit

---
+++ src/main/java/com/portugalruben/DIYDA/DiydaApplication.java
@@ -0,0 +1,13 @@
+package com.portugalruben.DIYDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiydaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiydaApplication.class, args);
+	}
+
+}
